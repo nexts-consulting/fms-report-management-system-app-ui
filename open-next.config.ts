8480bb748ace3c115fe2affed230815eb1d6d72f
kienle8475 <kienle.8475@gmail.com>
2026-01-25 15:09:07 +0700

    update open next config

--- open-next.config.ts
+++ open-next.config.ts
@@ -5,9 +5,22 @@ const config: OpenNextConfig = {
     override: {
       wrapper: 'cloudflare-node',
       converter: 'edge',
-      incrementalCache: 's3-lite',
-      queue: 'sqs-lite',
+      proxyExternalRequest: 'fetch',
+      incrementalCache: 'dummy',
       tagCache: 'dummy',
+      queue: 'dummy',
+    },
+  },
+  edgeExternals: ['node:crypto'],
+  middleware: {
+    external: true,
+    override: {
+      wrapper: 'cloudflare-edge',
+      converter: 'edge',
+      proxyExternalRequest: 'fetch',
+      incrementalCache: 'dummy',
+      tagCache: 'dummy',
+      queue: 'dummy',
     },
   },
 };
